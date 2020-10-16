f7df71212cdd14e837fcb7b2b05f2887832f3e02
Hong-Georgia-Data <66703368+Hong-Georgia-Data@users.noreply.github.com>
2020-10-16 19:38:18 -0400

    With Bootstrap. Hope that it wil work

--- pokemon_project/abbmap.js
+++ pokemon_project/abbmap.js
@@ -1,6 +1,7 @@
 // Submit Button handler
 
 function handleSubmit() {
+  
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
@@ -9,17 +10,277 @@ function handleSubmit() {
     console.log(pokemon_abi);
     var pokemon_moves = d3.select("#Moves").node().value.replace(/\s+/g, '-').toLowerCase();
     console.log(pokemon_moves);
-
+    var pokemon = d3.select("#Pokemon").node().value;
+    console.log(pokemon);
     // clear the input value
     d3.select("#Ability").node().value = "";
     d3.select("#Moves").node().value = "";
+    d3.select("#Pokemon").node().value = "";
 
     // Build the plot with the new stock
-    buildPlot(pokemon_abi);    buildPlot2(pokemon_moves);
+    buildPlot3(pokemon_abi);    buildPlot2(pokemon_moves);
+  
+  
+    // clear the input value
+  
+    // Build the plot with the new stock
+    buildPlot(pokemon);
+}
+///// Pokemon
+function buildPlot(pokemon) {
+  var url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
 
+  d3.json(url).then(function(data) {
+      console.log(data)
+      // Grab values from the response json object to build the plots
+      // var id = data.id
+      var poke_name=data.name
+
+      var ability_key = data.abilities
+      var abi_lis = []
+      for(i = 0; i < ability_key.length; i++) {abi_lis.push(ability_key[i].ability.name)}
+      console.log(abi_lis)
+      var move_key = data.moves
+      var move_ls = []
+      for(i = 0; i < move_key.length; i++) {move_ls.push(move_key[i].move.name)}
+      console.log(move_ls)
+
+function Chidren_1(list){
+  var list_names = [];
+  for (i=0;i<list.length;i++){
+      list_names.push({"name":list[i].toUpperCase()});
+  }
+  return list_names;
 }
 
-function buildPlot(pokemon_abi) {
+
+var x = Chidren_1(abi_lis)
+console.log(x);
+
+/////
+
+///
+
+var y = Chidren_1(move_ls)
+console.log(y)
+
+var treeData =
+{
+  "name": poke_name.toUpperCase(),
+  "children": [
+   
+      {   "name": 'Abilities' ,
+          "children": x 
+    }
+    ,
+    {   "name": "Moves" ,"children": y}
+  ]
+};
+
+
+// Set the dimensions and margins of the diagram
+var margin = {top: 20, right: 90, bottom: 30, left: 200},
+  // width = 960 - margin.left - margin.right,
+  // height = 500 - margin.top - margin.bottom;
+  width = 1600 - margin.left - margin.right,
+  height = 900 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.right + margin.left)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform", "translate("
+        + margin.left + "," + margin.top + ")");
+
+
+
+
+var i = 0,
+  duration = 750,
+  root;
+
+// declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d) { return d.children; });
+root.x0 = height / 2;
+root.y0 = 0;
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d) {
+if(d.children) {
+  d._children = d.children
+  d._children.forEach(collapse)
+  d.children = null
+}
+}
+
+function update(source) {
+
+// Assigns the x and y position for the nodes
+var treeData = treemap(root);
+
+// Compute the new tree layout.
+var nodes = treeData.descendants(),
+    links = treeData.descendants().slice(1);
+
+// Normalize for fixed-depth.
+nodes.forEach(function(d){ d.y = d.depth * 100}); //original is 180
+
+// ****************** Nodes section ***************************
+
+// Update the nodes...
+var node = svg.selectAll('g.node')
+    .data(nodes, function(d) {return d.id || (d.id = ++i); });
+
+
+// Enter any new modes at the parent's previous position.
+var nodeEnter = node.enter().append('g')
+    .attr('class', 'node')
+    .attr("transform", function(d) {
+      return "translate(" + source.y0 + "," + source.x0 + ")";
+  })
+  .on('click', click);
+
+
+// Add Circle for the nodes
+nodeEnter.append('circle')
+    .attr('class', 'node')
+    .attr('r', 1e-6)
+    .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+      // return d._children ? "red" : "yellow";
+    });
+
+// Add labels for the nodes
+nodeEnter.append('text')
+    .attr("dy", ".5em")
+    .attr("x", function(d) {
+      //   return d.children || d._children ? -13 : 13;
+        return d.children || d._children ? -13 : 13;
+
+    })
+    .attr("text-anchor", function(d) {
+        return d.children || d._children ? "end" : "start";
+    })
+    .attr(  "word-break", "break-all"
+    )
+    .text(function(d) { return d.data.name; });
+
+// UPDATE
+var nodeUpdate = nodeEnter.merge(node);
+
+// Transition to the proper position for the node
+nodeUpdate.transition()
+  .duration(duration)
+  .attr("transform", function(d) { 
+      return "translate(" + d.y + "," + d.x + ")";
+   });
+
+// Update the node attributes and style
+nodeUpdate.select('circle.node')
+  .attr('r', 5)
+  .style("fill", function(d) {
+      return d._children ? "lightsteelblue" : "yellow";
+  })
+  .attr('cursor', 'pointer');
+
+
+// Remove any exiting nodes
+var nodeExit = node.exit().transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+        return "translate(" + source.y + "," + source.x + ")";
+    })
+    .remove();
+
+// On exit reduce the node circles size to 0
+nodeExit.select('circle')
+  .attr('r', 1e-6);
+
+// On exit reduce the opacity of text labels
+nodeExit.select('text')
+  .style('fill-opacity', 1e-6);
+  // .style('fill-opacity', 2);
+
+// ****************** links section ***************************
+
+// Update the links...
+var link = svg.selectAll('path.link')
+    .data(links, function(d) { return d.id; });
+
+// Enter any new links at the parent's previous position.
+var linkEnter = link.enter().insert('path', "g")
+    .attr("class", "link")
+    .attr('d', function(d){
+      var o = {x: source.x0, y: source.y0}
+      return diagonal(o, o)
+    });
+
+// UPDATE
+var linkUpdate = linkEnter.merge(link);
+
+// Transition back to the parent element position
+linkUpdate.transition()
+    .duration(duration)
+    .attr('d', function(d){ return diagonal(d, d.parent) });
+
+// Remove any exiting links
+var linkExit = link.exit().transition()
+    .duration(duration)
+    .attr('d', function(d) {
+      var o = {x: source.x, y: source.y}
+      return diagonal(o, o)
+    })
+    .remove();
+
+// Store the old positions for transition.
+nodes.forEach(function(d){
+  d.x0 = d.x/2;
+  d.y0 = d.y/2;
+});
+
+// Creates a curved (diagonal) path from parent to the child nodes
+function diagonal(s, d) {
+
+  path = `M ${s.y} ${s.x}
+          C ${(s.y + d.y) / 2} ${s.x},
+            ${(s.y + d.y) / 2} ${d.x},
+            ${d.y} ${d.x}`
+
+  return path
+}
+
+// Toggle children on click.
+function click(d) {
+  if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+  update(d);
+}
+
+
+
+
+
+}
+  })
+}
+
+function buildPlot3(pokemon_abi) {
     var url = `https://pokeapi.co/api/v2/ability/${pokemon_abi}`;
 
     d3.json(url).then(function(data) {
@@ -253,7 +514,7 @@ function update(source) {
 
   // Enter any new links at the parent's previous position.
   var linkEnter = link.enter().insert('path', "g")
-      .attr("class", "link")
+      .attr("class", "link",)
       .attr('d', function(d){
         var o = {x: source.x0, y: source.y0}
         return diagonal(o, o)
@@ -332,8 +593,9 @@ function buildPlot2(pokemon_moves) {
             var damage = data.damage_class
             var target = data.target
             var type = data.type
-            var effect_chance=data.effect_chance
-
+            var chance=data.effect_chance
+            var effect_chance = 0
+            if (chance = "null") {effect_chance = 100;} else { effect_chance = chance;}
             var move_name=data.name
   
     
@@ -600,7 +862,21 @@ function buildPlot2(pokemon_moves) {
       }
     }
         })}
+
+
+
 // Add event listener for submit button
 d3.select("#submit").on("click", handleSubmit);
+
+// d3.select("#submit").on("keypress", function() {
+//   if(d3.event.keyCode === 32 || d3.event.keyCode === 13)
+// })
+
+// if (d3.event.keyCode === 32 || d3.event.keyCode === 13) {
+//   //  block of code to be executed if condition1 is true
+// } else if (condition2) {
+//   //  block of code to be executed if the condition1 is false and condition2 is true
+// } 
+// }
 // </script>
 // </body>
